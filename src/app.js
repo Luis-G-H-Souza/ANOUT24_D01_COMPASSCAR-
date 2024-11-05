d126762c2fe47dd98cebdcd42fc14cb5acba2074
Luis <luisouza.gu10@gmail.com>
2024-11-05 14:12:39 -0300

    feat: implementing basic structure

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const app = require('express')()
+const consign = require('consign')
+
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middleware.js')
+  .into(app)
+
+app.get('/', (req, res) => {
+  res.status(200).send()
+})
+
+module.exports = app

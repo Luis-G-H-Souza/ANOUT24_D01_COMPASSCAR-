03103e7afaf91a333168c14bf4eca08db6cddecd
Luis <luisouza.gu10@gmail.com>
2024-11-11 15:48:40 -0300

    fix: Fixing items error messages

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,16 +1,20 @@
 module.exports = (app) => {
   const put = (req, res) => {
-    app.services.item.update(req.params.id, req.body)
-      .then((updateItem) => {
-        if (updateItem) {
-          if (updateItem.error === 'car not found') {
-            return res.status(404).json(updateItem.error)
-          } else if (updateItem.error) {
-            return res.status(400).json(updateItem.error)
+    try {
+      app.services.item.update(req.params.id, req.body)
+        .then((updateItem) => {
+          if (updateItem) {
+            if (updateItem.error === 'car not found') {
+              return res.status(404).json(updateItem.error)
+            } else if (updateItem.error) {
+              return res.status(400).json(updateItem.error)
+            }
           }
-        }
-        res.status(204).json(updateItem)
-      })
+          res.status(204).json(updateItem)
+        })
+    } catch (error) {
+      res.status(500).json({ errors: ['an internal server error occurred'] })
+    }
   }
 
   return { put }
